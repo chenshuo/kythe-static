ac1adf318ba394c4e4f5e5d0e71b2863669b9e5a
Shuo Chen <chenshuo@chenshuo.com>
2016-04-23 13:27:39 -0700

    update test0.cc

--- testdata/test0.cc
+++ testdata/test0.cc
@@ -1,8 +1,42 @@
+struct data
+{
+  int x;
+  double y;
+};
 
 void foo();
-void foo(int);
+int foo(int);
+
+class Base
+{
+ public:
+  virtual ~Base()
+  {
+  }
+
+  virtual void process(data* d) = 0;
+};
+
+class Derived : public Base
+{
+ public:
+  void process(data* d) override
+  {
+    d->y = 19 * 82.0;
+  }
+};
 
 int main()
 {
   foo();
+  data d;
+  d.x = 1;
+
+  Derived p;
+  p.process(&d);
+
+  Base* pb = &p;
+  pb->process(&d);
+
+  auto x = foo(123);
 }
